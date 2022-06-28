c43655855ff9186b73c083f7ab0fb7ae6939219a
wukaiwei1 <2596862515@qq.com>
2022-06-28 20:38:42 +0800

    v-model修饰符

---
+++ vue-cli/src/v-model修饰符.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <span>年龄</span>：
+      <input type="text" v-model.number="age" />
+    </div>
+    <div>
+      <span>人生格言</span>：
+      <input type="text" v-model.trim="motto" />
+    </div>
+    <div>
+      <span>自我介绍:</span>
+      <textarea v-model.lazy="intro"></textarea>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      motto: "",
+      age: "",
+      info: "",
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
