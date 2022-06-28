f3b536c6d69d26d67bb1974b80c479d6cac45efe
wukaiwei1 <2596862515@qq.com>
2022-06-28 20:09:41 +0800

    v-on事件修饰符

---
+++ vue-cli/src/v-on事件对象.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <a @click="add" href="https://www.baidu.com/">阻止跳转百度</a>
+
+    <button @click="addFn(5, $event)">事件对象</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    add(e) {
+      e.preventDefault();
+    },
+    addFn(val, e) {
+      e.preventDefault();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
